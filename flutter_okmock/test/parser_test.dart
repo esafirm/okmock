28b554a2f1b607f193d1fbb00db99cd7c4e23cea
Esa Firman <esafirm21@gmail.com>
2021-07-17 11:04:11 +0700

    use the test library

--- flutter_okmock/test/parser_test.dart
+++ flutter_okmock/test/parser_test.dart
@@ -1,13 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:okmock/OkMockPayload.dart' as OkMock;
 
 void main() {
-  // create map with key and value
-  final matcherJson = new Map<String, Object>();
-  matcherJson["path"] = "google.com";
-  matcherJson["method"] = "GET";
+  test('parsing should be correct', () {
+    // create map with key and value
+    final matcherJson = new Map<String, Object>();
+    matcherJson["path"] = "google.com";
+    matcherJson["method"] = "GET";
 
-  final matcher = OkMock.Matcher.fromJson(matcherJson);
+    final matcher = OkMock.Matcher.fromJson(matcherJson);
 
-  assert(matcher.path.hasMatch("google.com"));
-  assert(matcher.method == matcherJson["method"]);
+    assert(matcher.path.hasMatch("google.com"));
+    assert(matcher.method == matcherJson["method"]);
+  });
 }
